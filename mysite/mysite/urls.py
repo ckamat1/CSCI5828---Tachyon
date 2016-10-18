efaef6712afa0938e80572c2b0ad6e0232199c86
Chirag Kamat <chka1976@colorado.edu>
2016-10-17 17:28:23 -0700

    adding index.html

--- mysite/mysite/urls.py
+++ mysite/mysite/urls.py
@@ -8,8 +8,8 @@ admin.autodiscover()
 
 urlpatterns = [url(r'^register/', views.register, name='register'),
                url(r'^viewrecords/',views.printdata, name='print_data'),
-               # url(r'^$', views.index, name='home'),
-               url(r'^$',views.home, name='home'),
+               url(r'^$', views.index, name='home'),
+               # url(r'^$',views.home, name='home'),
                # url(r'^faculty/', TemplateView.as_view(template_name='faculty.html'),
                #        name='faculty'),
                url(r'^admin/', include(admin.site.urls))]
