0fea988f6b02a6c5585a04c67b8075870921fbc7
Chirag Kamat <chka1976@colorado.edu>
2016-12-09 23:08:09 -0800

    Changes to the algorithm

--- mysite/dlcapp/Algorithm.py
+++ mysite/dlcapp/Algorithm.py
@@ -2,55 +2,52 @@ from .models import Project, Student, Faculty
 from django.db.models import Q
 import re
 
-class Student:
+class stud:
     def __init__(self, app_ID):
         self.app_ID = app_ID
         self.priority = 0
 
+def getValidStudentAndProjects():
+    eligible_students = Student.objects.filter(gpa__gte='3.0').filter(pastdla_experience__contains='No') \
+        .filter(student_commitment__contains='Yes').filter(student_ispart_engineering__contains='Yes')
+    # Need to eliminate students who have past DLA experience, who are grad students and who are not a part of CU engineering.
+    project_query = Project.objects.all()
+    student_query = eligible_students
+    valid_projects = []
+    for entry in project_query:
+        query_set = student_query.filter(Q(project1__contains=entry.title) | Q(project2__contains=entry.title)
+                                         | Q(project3__contains=entry.title) | Q(project4__contains=entry.title)
+                                         | Q(project5__contains=entry.title))
+        if query_set.exists():
+            valid_projects.append(entry)
+    return eligible_students,valid_projects
 
 
 
 class Algorithm(object):
     def __init__(self):
-        self.eligible_students = None
-        self.valid_projects = None
-
-    def getValidStudents(self):
-        eligible_students = Student.objects.filter(gpa__gte = '3.0').filter(pastdla_experience__contains = 'No')\
-            .filter(student_commitment__contains = 'Yes').filter(student_ispart_engineering__contains = 'Yes')
-        #Need to eliminate students who have past DLA experience, who are grad students and who are not a part of CU engineering.
-        self.eligible_students = eligible_students
-        return eligible_students
-
-    def getValidProjects(self):
-        project_query = Project.objects.all()
-        student_query = self.eligible_students
-        valid_projects = []
-        # project1 =
-        for entry in project_query:
-            query_set = student_query.filter(Q(project1__contains=entry.title)|Q(project2__contains=entry.title)
-                                             |Q(project3__contains=entry.title)|Q(project4__contains=entry.title)
-                                             |Q(project5__contains=entry.title))
-            if query_set.exists():
-                valid_projects.append(entry)
-        self.valid_projects = valid_projects
-        return valid_projects
-
+        self.eligible_students,self.valid_projects = getValidStudentAndProjects()
+        # self.valid_projects = None
 
     def Match(self):
-        valid_projects = self.valid_projects
+        query_results = self.valid_projects
         eligible_students = self.eligible_students
         #student_with_max_GPA = 0.0
         student_level = {'Senior':4,'Junior':3,'Sophomore':2,'Freshman':1}
-        for entry in valid_projects:
-            entry.students = eligible_students.filter(Q(project1__contains=entry.title)|Q(project2__contains=entry.title)
+
+        for entry in query_results:
+            entry.student = eligible_students.filter(Q(project1__contains=entry.title)|Q(project2__contains=entry.title)
                                              |Q(project3__contains=entry.title)|Q(project4__contains=entry.title)
                                              |Q(project5__contains=entry.title))
-
-            for student in entry.students:
-                std = Student(student.application_id)
-                std.priority += student.gpa
-                std.priority += student_level[student.school_level]
+            # entry.student_selected = "NA"
+            entry.student_assigned = "NA"
+            students_dict = {'Student':None,'Priority':0}
+            # priority = 0
+            for student in entry.student:
+                std = stud(student.application_id)
+                std.priority += float(student.gpa)
+                if student.school_level:
+                    std.priority += student_level[student.school_level]
                 if student.previous_dlc_apply == "Yes":
                     std.priority += 1
                 if entry.desired_student_id == student.name:
@@ -69,6 +66,18 @@ class Algorithm(object):
                     std.priority += 2
                 else:
                     std.priority += 1
+
+                if std.priority > students_dict['Priority']:
+                    students_dict['Student'] = std
+                    students_dict['Priority'] = std.priority
+
+            final_student = students_dict['Student']
+            student_selected =  entry.student.get(application_id=final_student.app_ID)
+            # entry.student_selected = student_selected.name
+            entry.student_assigned = student_selected.name
+            entry.save()
+        return query_results
+
 
 
 
