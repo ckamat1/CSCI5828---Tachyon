7420c09dd2c38ede54ca25cc245e2b6a36e5785d
Chirag Kamat <chka1976@colorado.edu>
2016-12-09 18:16:09 -0800

    ALgorithm changes. Added constraints and assigned student priorities

--- mysite/dlcapp/Algorithm.py
+++ mysite/dlcapp/Algorithm.py
@@ -1,9 +1,10 @@
 from .models import Project, Student, Faculty
 from django.db.models import Q
+import re
 
 class Student:
-    def __init__(self, studentID):
-        self.studentID = studentID
+    def __init__(self, app_ID):
+        self.app_ID = app_ID
         self.priority = 0
 
 
@@ -39,6 +40,43 @@ class Algorithm(object):
     def Match(self):
         valid_projects = self.valid_projects
         eligible_students = self.eligible_students
+        #student_with_max_GPA = 0.0
+        student_level = {'Senior':4,'Junior':3,'Sophomore':2,'Freshman':1}
+        for entry in valid_projects:
+            entry.students = eligible_students.filter(Q(project1__contains=entry.title)|Q(project2__contains=entry.title)
+                                             |Q(project3__contains=entry.title)|Q(project4__contains=entry.title)
+                                             |Q(project5__contains=entry.title))
+
+            for student in entry.students:
+                std = Student(student.application_id)
+                std.priority += student.gpa
+                std.priority += student_level[student.school_level]
+                if student.previous_dlc_apply == "Yes":
+                    std.priority += 1
+                if entry.desired_student_id == student.name:
+                    std.priority += 1
+                if student.gender == "female":
+                    std.priority += 1
+                if student.ethnonym == "Yes":
+                    std.priority += 1
+                if student.project1 == entry.title:
+                    std.priority += 5
+                elif student.project2 == entry.title:
+                    std.priority += 4
+                elif student.project3 == entry.title:
+                    std.priority += 3
+                elif student.project4 == entry.title:
+                    std.priority += 2
+                else:
+                    std.priority += 1
+
+
+
+
+
+
+
+
 
 
 
